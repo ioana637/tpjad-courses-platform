ff175f190827f8b2fe200ead9c2bec2777a8e279
unknown <ioanaclaudia137@gmail.com>
2020-02-02 14:53:55 +0200

    course-service

--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpHeaders, HttpClient } from '@angular/common/http';
+import { coursesGetAll, coursesAddEdit, coursesSendNewsToStudents, coursesFilter, coursesMy, coursesDeleteAndGetById, coursesEnroll, coursesStudentsForCourse, coursesNumberStudents } from './urls';
+import { Course } from '../utils/structures';
 
 @Injectable({
   providedIn: 'root'
@@ -15,5 +17,39 @@ export class CoursesService {
     });
   }
 
-  
+  fetchAllCourses() {
+    return this.http.get(coursesGetAll, {headers: this.httpHeaders});
+  }
+
+  saveCourse(course: Course) {
+    return this.http.post(coursesAddEdit, JSON.stringify(course), {headers: this.httpHeaders});
+  }
+
+  sendMailToStudents(obj: {courseId: number, message: string}){
+    return this.http.post(coursesSendNewsToStudents, JSON.stringify(obj), {headers: this.httpHeaders});
+  }
+
+  filterCourses(text: string) {
+    return this.http.get(`${coursesFilter}?filter=${text}`,{headers: this.httpHeaders} );
+  }
+
+  getMyCourses() {
+    return this.http.get(coursesMy, {headers: this.httpHeaders});
+  }  
+
+  getCourseById(courseId: number) {
+    return this.http.get(coursesDeleteAndGetById(courseId.toString()), {headers: this.httpHeaders} );
+  }
+
+  enrollToCourse(courseId: number) {
+    return this.http.post(coursesEnroll(courseId.toString()), null, {headers: this.httpHeaders} );
+  }
+
+  getStudentsForCourse(courseId: number) {
+    return this.http.get(coursesStudentsForCourse(courseId.toString()), {headers: this.httpHeaders} );
+  }
+
+  getNumberStudentsForCourse(courseId: number) {
+    return this.http.get(coursesNumberStudents(courseId.toString()), {headers: this.httpHeaders} );
+  }
 }
